be3ac697c0b70a9e0fcdc0fc74aafae795b1fd07
Rollno45 <60298503+Rollno45@users.noreply.github.com>
2022-02-15 12:03:38 +0530

    dd
    dd

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,10 @@
 import React from "react";
 import '../styles/App.css';
 import Welcome from './Welcome';
-
 const App = () => {
+  let name = "Satish";
   return (
-    <Welcome name="" />
+    <Welcome name={name} />
   )
 }
-
-
 export default App;
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,1 +1,21 @@
 // write code for Welcome component here
+import React from "react";
+import '../styles/App.css';
+import App from "./App";
+const Welcome = ({name}) => {
+    let sayHi = `Hey ${name}!`;
+    let greetMessage = `Welcome to Newton School.`;
+    return (
+        <div>
+            <h1>{sayHi}</h1>
+            <h2>{greetMessage}</h2>
+        </div>
+    );
+}
+const functionExample=()=>
+{
+    return (
+    <Welcome/>
+    );
+}
+export default Welcome;
